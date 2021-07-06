497c1b455f72e712d5df683d7966d02d1e192bc1
Julius Snipes <jsnipes@fb.com>
2021-07-06 17:17:05 -0400

    User can post image and description to Parse server

--- app/src/main/java/com/example/parstagram/ParseApplication.java
+++ app/src/main/java/com/example/parstagram/ParseApplication.java
@@ -3,6 +3,7 @@ package com.example.parstagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
@@ -12,6 +13,7 @@ public class ParseApplication extends Application {
         super.onCreate();
 
         // Register parse models
+        ParseObject.registerSubclass(Post.class);
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("Uf07k9UJ01WQPLRKO2cXj4oY0giVBM01ByKeB7W0")
                 .clientKey("B67BE5RedrZIs250WHD2LYkE7uu2ls6dHAqla9Kk")
