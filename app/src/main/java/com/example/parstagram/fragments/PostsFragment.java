547cdbaa6257303f0722c4446ffa89d7b969ac84
Julius Snipes <jsnipes@fb.com>
2021-07-06 20:23:44 -0400

    User can view other posts

---
+++ app/src/main/java/com/example/parstagram/fragments/PostsFragment.java
@@ -0,0 +1,83 @@
+package com.example.parstagram.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.parstagram.Post;
+import com.example.parstagram.PostsAdapter;
+import com.example.parstagram.R;
+import com.example.parstagram.databinding.FragmentComposeBinding;
+import com.example.parstagram.databinding.FragmentPostsBinding;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class PostsFragment extends Fragment {
+
+    FragmentPostsBinding binding;
+    public static final String TAG = "PostsFragment";
+    private static final int LIMIT = 20;
+    private PostsAdapter adapter;
+    private List<Post> posts;
+
+    public PostsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentPostsBinding.inflate(getLayoutInflater(), container, false);
+        View view = binding.getRoot();
+        // Inflate the layout for this fragment
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        posts = new ArrayList<>();
+        adapter = new PostsAdapter(getContext(), posts);
+
+        binding.rvPosts.setAdapter(adapter);
+        binding.rvPosts.setLayoutManager(new LinearLayoutManager(getContext()));
+        queryPosts();
+
+
+    }
+
+    private void queryPosts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.setLimit(LIMIT);
+        query.addDescendingOrder(Post.KEY_CREATED_AT);
+        query.findInBackground((posts, e) -> {
+            if (e != null) {
+                Log.e(TAG, "Couldn't get post", e);
+                return;
+            }
+            for (Post post: posts) {
+                Log.i(TAG, "Post: " + post.getDescription() + ", username: " + post.getUser().getUsername());
+            }
+            this.posts.addAll(posts);
+            adapter.notifyDataSetChanged();
+        });
+    }
+}
