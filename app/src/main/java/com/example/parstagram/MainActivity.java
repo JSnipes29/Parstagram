d06dfbdf9c06d588330f8e9c157862f0b1af4b10
Julius Snipes <jsnipes@fb.com>
2021-07-08 20:31:40 -0400

    User can add profile image

--- app/src/main/java/com/example/parstagram/MainActivity.java
+++ app/src/main/java/com/example/parstagram/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -36,13 +37,15 @@ import com.parse.SaveCallback;
 
 import org.parceler.Parcels;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
 
-
+    private static final int GALLERY_REQUEST = 14;
     ActivityMainBinding binding;
     public static final String TAG = "MainActivity";
     final FragmentManager fragmentManager = getSupportFragmentManager();
@@ -98,6 +101,34 @@ public class MainActivity extends AppCompatActivity {
         return super.onPrepareOptionsMenu(menu);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            switch (requestCode) {
+                case GALLERY_REQUEST:
+                    Uri selectedImage = data.getData();
+                    try {
+                        Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+                        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                        // Compress image to lower quality scale 1 - 100
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+                        byte[] image = stream.toByteArray();
+
+
+                        ParseUser user = ParseUser.getCurrentUser();
+                        ParseFile parseFile = new ParseFile(user.getUsername() + "_pic.jpeg", image);
+                        ParseUser.getCurrentUser().put("profileImage", parseFile);
+                        ParseUser.getCurrentUser().saveInBackground();
+                        Toast.makeText(this, "You have updated your profile image", Toast.LENGTH_SHORT).show();
+                    } catch (IOException e) {
+                        Log.e(TAG, "Problem with uploading profile image",e);
+                    }
+                    break;
+            }
+    }
+    }
+
     public void logout(MenuItem mi) {
         ParseUser.logOut();
         Intent i = new Intent(MainActivity.this, LoginActivity.class);
@@ -135,6 +166,15 @@ public class MainActivity extends AppCompatActivity {
         // Hide progress item
         miActionProgressItem.setVisible(false);
     }
+
+    public void changeProfileImage(MenuItem mi) {
+        Log.i(TAG, "Clicked profile image menu item");
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, GALLERY_REQUEST);
+        Log.i(TAG, "activity started");
+    }
+
 
 
 }
